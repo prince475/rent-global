91c4d3271aa69c8257d500b2d9167c37f321e4d2
prince475 <danielmwai475@gmail.com>
2023-10-26 23:57:55 +0300

    Restructuring index.js file

---
+++ src/main-page/index.js
@@ -0,0 +1,13 @@
+import './main-page.css'
+import  Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+        
+     <Header subtitle="Providing our clients with not just a house but a Home!"/>
+    </div>
+  );
+}
+
+export default App;
