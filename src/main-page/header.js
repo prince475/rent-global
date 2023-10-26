0b108cbe454362728219c9617f6a30d79f77c7c7
prince475 <danielmwai475@gmail.com>
2023-10-26 23:58:18 +0300

    Adding main styling

---
+++ src/main-page/header.js
@@ -0,0 +1,20 @@
+import logo from './Globologo.png'
+import './App.css'
+
+//components are arguments that are passed into the component
+
+const Header = ({subtitle}) => 
+   (
+    <header className='row'>
+        
+        <div className='col-md-5'>
+            <img src={logo} className='logo' alt='logo'/>
+        </div>
+
+        <div className='col-md-7 mt-5 subtitle'>
+        {subtitle}
+        </div>
+    </header>
+  )
+
+  export default Header; 
