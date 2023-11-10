e0f1826ea28c079ed437eee1eb5604f4b58e0ef4
prince475 <danielmwai475@gmail.com>
2023-11-10 13:38:03 +0300

    Adding search functionality and useHistory hook to always remember which navigation we are in

--- src/main-page/HouseFilter.js
+++ src/main-page/HouseFilter.js
@@ -1,15 +1,27 @@
+import { useHistory } from "react-router-dom";
+
 const HouseFilter = ({allHouses}) => {
+    const history = useHistory();
     const countries = allHouses
     ? Array.from(new Set(allHouses.map((h) => h.country)))
     : [];
     countries.unshift(null)
+
+    // Navigation with the History Hook
+    const onSearchChange = (e) => {
+        const country = e.target.value;
+        history.push(`/searchresults/${country}`) 
+    }
+    
+
+
     return (
         <div className="row mt-3">
             <div className="offset-md-2 col-md-4">
                 Look for your dream house in the country
             </div>
             <div className="col-md-4 mb-3">
-                <select className="form-select">
+                <select className="form-select" onChange={onSearchChange}>
                     {countries.map((c) => (
                         <option key={c} value={c}>
                             {c}
